b46b62eb11650b67b1541f420ff0ee77d1eac3a3
Вадим Монада <vammaa2017@gmail.com>
2025-06-27 14:48:21 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,3 +4,23 @@ const menu = document.getElementById("header__ul");
 burger.addEventListener("click", () => {
   header__ul.classList.toggle("active");
 });
+
+$(document).ready(function () {
+  $('.product__slider').slick({
+    arrows: false,
+    dots: false,
+    adaptiveHeight: false,
+    slidesToShow: 3,
+    centerMode: true,
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 1100,
+        settings: {
+          variableWidth: true,
+          slidesToShow: 1,
+        }
+      }
+    ]
+  });
+});
